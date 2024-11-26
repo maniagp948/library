9a71de6eb6de1ae819fb422cb3f1c19e7b855e98
Gabriel Mania <maniagp@proton.me>
2024-11-26 11:38:03 +0800

    Add Book to admin page

--- books/models.py
+++ books/models.py
@@ -7,6 +7,5 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
 
-
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
